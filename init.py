b1cefbdf8bdb39b1badf7bab12f7ce90ee5b2b3f
Roland-Sankara <roland1sankara@gmail.com>
2025-03-25 16:17:11 +0300

    add account functionality

--- init.py
+++ init.py
@@ -1,72 +1,147 @@
 import datetime
 
-ac_numbers = ['jk25', 'jdf26']
+ac_numbers = {}
 
-# SACCO Member class
-class SACCO_Member:
-    def __init__(self, name, farmer_id, age, occupation, address, contact, next_of_kin):
-        self.name = name
-        self.farmer_id = farmer_id
-        self.age = age
-        self.occupation = occupation
-        self.address = address
-        self.contact = contact
-        self.next_of_kin = next_of_kin
+def farmer_account_auth(id):
+    try:
+        print("Account Exists")  
+        return ac_numbers[farmer_id]
+    except:
+        print("No account with given id")
+    
+# sacco_members = []
+
+
 
 class Account:
     def __init__(self, id):
         self.id = id
         self.balance = 0
         self.authCheck = False
+        self.deposits = []
+        self.transactions = [] # has both withdrawal and deposits
+        self.created_at = datetime.datetime.now()
 
-    def farmer_id_auth(self):
-        farmer_id_acounts = input("Enter Farmer ID\n")
-        
-        for ac in ac_numbers:
-            if(farmer_id_acounts == ac):
-                print("Auth Success")
-                self.authCheck = True
-                break
-            else:
-                print("Auth Fail")
-                self.authCheck = False
-        
-        
+    # Create a DS for the deposits to ensure efficient insertion & retrieval
+    # Use a Hash Map - for quick lookup of farmer's deposit history
+    # Maintain and ordered list of deposits (use stack)
     def deposit_money(self):
-        if(self.authCheck):
-            amount = input("Enter Amount\n")
-            time_stamp = datetime.datetime.now()
-            print({
+        amount = input("Enter Deposit Amount\n")
+
+        if int(amount) > 0:
+            depositInfo = {
+                "farmer_id": self.id,
                 "amount": amount,
-                "timeStamp": time_stamp
-            })
-
+                "timeStamp": datetime.datetime.now(),
+                "transaction_type": "Deposit"
+            }
+        
             self.balance += int(amount)
-        else:
-            print("Invalid Account Number")
 
+            self.deposits.insert(0, depositInfo)
+            self.transactions.insert(0, depositInfo)
+        else: 
+            print("Deposit should be greater than zero")
+        
+
+    # Check amount before withdraw
+    # Store & update farmer balances in constant time O(1)
+    # Retrieving the most recent transcations first (Use Stack)
+    # Store transaction with farmerID, Amount & Timestamp.
+    def withdraw_money(self):
+        # How much do you want to withdraw
+        amount = input("Enter Withdraw Amount\n")
+        # check if ammount is available
+        if int(amount) <= self.balance:
+            print("Withdrawing...")
+
+            withdrawInfo = {
+                "farmer_id": self.id,
+                "amount": amount,
+                "timeStamp": datetime.datetime.now(),
+                "transaction_type": "Withdraw"
+            }
+
+            self.balance -= int(amount)
+            self.transactions.insert(0, withdrawInfo)
+
+        else:
+            print("Insufficient Account Balance")
+
+    
+    def get_account_balance(self):
+        print(f"Your Balance is = {self.balance}")
+
+    def get_account_statement(self):
+        print("---STATEMENT-START---")
+        for transaction in self.transactions:
+            print(f"{transaction}\n")
+        print("---END---")
 
 # Instances
-# farmerOne = SACCO_Member("Roland", "22343JK", 20, "Engineer", "Kampala",)
-# newAccount = Account("JK25")
-# print(newAccount.id)
-# newAccount.deposit_money()
 
-# yoAccount = Account("JFD26")
-# yoAccount.deposit_money()
+# +++++++++++++++++++++++
+# Interface for running app
+# +++++++++++++++++++++++
 
-# print(f"New's Account Balance = {newAccount.balance}")
-# print(f"Yo's Account Balance = {yoAccount.balance}")
+running_state = True
 
-# if isinstance(yoAccount, Account):
-#     print("Yes")
-# else:
-#     print("Not an instance")
+while(running_state):
+    # create SACCO Member
+    member_status = input("\nDo you have a SACCO Account? Yes/No\n")
 
-testAccount = Account("JFD26")
+    if(member_status == "Yes"):
 
-testAccount.farmer_id_auth()
-testAccount.deposit_money()
+        # Authentication
+        farmer_id = input("\nEnter your FarmerID\n")
+        # check is the farmerID entered has an account setup
+        farmer_account = farmer_account_auth(farmer_id)
+
+        # Choose a Service
+        service_req = input("\nChoose Service\n 1. Deposit\n 2. Withdraw\n 3. Check Balance\n 4. Get Statement\n")
+        
+        match service_req:
+            case '1':
+                farmer_account.deposit_money()
+            case '2':
+                farmer_account.withdraw_money()
+            case '3':
+                farmer_account.get_account_balance()
+            case '4':
+                farmer_account.get_account_statement()
+            case _:
+                print("Invalid Option.. Try again\n")
+
+    else:
+       print("\n**Account Creation Process...")
+       farmer_id = input("Enter your given SACCO/FarmerID\n")
+       new_account = Account(farmer_id)
+       ac_numbers[farmer_id] = new_account
+
+       print("\nCongs.. Account Created. \nUse FarmerId to access account")
+       print("______________________\n")
+       
+    
+    stop_check = input("Continue or Exit?\n")
+    if(stop_check == "Exit"):
+        running_state = False
+    
+
+
+# INGORE CODE BELOW
+
+
+
+# SACCO Member class
+# class SACCO_Member:
+#     def __init__(self, name, farmer_id, age, occupation, address, contact, next_of_kin):
+#         self.name = name
+#         self.farmer_id = farmer_id
+#         self.age = age
+#         self.occupation = occupation
+#         self.address = address
+#         self.contact = contact
+#         self.next_of_kin = next_of_kin
 
 
 
